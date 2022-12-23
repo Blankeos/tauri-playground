8c83e83456b7bbdc1f7ca25f5de964807e859aa6
Blankeos <carloantonioct@gmail.com>
2022-12-23 23:55:57 +0800

    Custom tauri command test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,6 +9,11 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn sum(num1: &i32, num2: &i32) -> i32 {
+    num1 + num2
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
